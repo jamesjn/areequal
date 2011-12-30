e1fa5fe40c05a125b682058b1ae290f8347852b2
james c <james.jn.chiang@gmail.com>
2011-12-30 15:35:03 +0000

    Initial Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Areequal::Application.config.secret_token = '8c9681db95d90d65d33a619d4a6193871f1fc8f0d4453b2213cf43f72460a6ac03d58f59e075a3dbd5205a7f8a4306803efc38a98e0dda5447bc4a58cf7390df'
