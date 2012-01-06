842bf0494cd4b737a6a23b23a893b242306ba8e6
james c <james.jn.chiang@gmail.com>
2012-01-06 02:33:23 +0000

    Add edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def edit_profile
+    current_user_id = session[:user_id]
+    redirect_to edit_user_path(current_user_id) 
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user_params = params[:user]
+    user_params["id"] = session[:user_id]
+    current_user_id = session[:user_id]
+    User.update(current_user_id, user_params)
+    flash[:saved] = true
+    redirect_to edit_user_path(current_user_id) 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Areequal::Application.routes.draw do
 	resources :users
   resources :arts
+  match "/edit_profile" => "users#edit_profile"
   match "/login" => "landing#login" 
   match "/logout" => "landing#logout" 
   match "/about" => "landing#about" 
-  match "/browse" => "art#browse" 
+  match "/browse" => "arts#browse" 
   root :to => "landing#index"
 end
